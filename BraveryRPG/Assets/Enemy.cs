c10b72d3ac61880fd3d9cf6e4279bbec1e843e23
Kevin S <kevinschanz@live.com>
2025-06-01 17:15:52 -0400

    Change Enemy Color from Red to White after simple Countdown Timer
    Feats:
    - Use the Time.deltaTime and Mathf.Max utilities to perform a simple
      1 second countdown, to toggle the Enemy's color from Red back to White
      after they are hit by the `playerAttack` event

--- BraveryRPG/Assets/Enemy.cs
+++ BraveryRPG/Assets/Enemy.cs
@@ -2,8 +2,47 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    private SpriteRenderer sr;
+    [SerializeField] private float redColorDuration = 1;
+
+    public float timer;
+
+    private void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        ChangeColorIfNeeded();
+    }
+
+    private void ChangeColorIfNeeded()
+    {
+        timer -= Time.deltaTime;
+        timer = Mathf.Max(0, timer);
+
+        if (timer <= 0 && sr.color != Color.white)
+        {
+            TurnWhite();
+        }
+    }
+
     public void TakeDamage()
     {
         Debug.Log(gameObject.name + " took some damage!");
+
+        sr.color = Color.red;
+
+        // Alternative example to Invoke function after time elapsed:
+        // Invoke(nameof(TurnWhite), redColorDuration);
+
+        // Reset cooldown timer back to default.
+        timer = redColorDuration;
+    }
+
+    private void TurnWhite()
+    {
+        sr.color = Color.white;
     }
 }
