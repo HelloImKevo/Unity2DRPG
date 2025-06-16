1891392bef8df6bdeb6bc32f19eb84172eaa64db
Kevin S <kevinschanz@live.com>
2025-06-16 16:18:34 -0400

    Very minor project cleanup

--- BraveryRPG/Assets/Scripts/Enemies/Enemy_VFX.cs
+++ BraveryRPG/Assets/Scripts/Enemies/Enemy_VFX.cs
@@ -5,5 +5,10 @@ public class Enemy_VFX : Entity_VFX
     [Header("Counterattack Window")]
     [SerializeField] private GameObject attackAlert;
 
-    public void EnableAttackAlert(bool enable) => attackAlert.SetActive(enable);
+    public void EnableAttackAlert(bool enable)
+    {
+        if (attackAlert == null) return;
+
+        attackAlert.SetActive(enable);
+    }
 }
