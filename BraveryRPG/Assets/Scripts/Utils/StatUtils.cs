f05538b9fc8baadcd4289e6fa0375530bce5dd28
Kevin S. <kevinschanz@live.com>
2025-07-06 17:29:34 -0400

    Build a Scrollable Player Stats Window to display all Calculated Stats (#21)
    Summary:
    - Add Amulet of Doom
    - Add Wizard Robes
    - Make use of Scroll Rectangle, Grid Layout Group and Content Size Filter

---
+++ BraveryRPG/Assets/Scripts/Utils/StatUtils.cs
@@ -0,0 +1,54 @@
+public class StatUtils
+{
+    public static string GetStatNameByType(StatType type)
+    {
+        switch (type)
+        {
+            case StatType.MaxHealth: return "Max Health";
+            case StatType.HealthRegen: return "Health Regeneration";
+            case StatType.Armor: return "Armor";
+            case StatType.Evasion: return "Evasion";
+
+            case StatType.Strength: return "Strength";
+            case StatType.Agility: return "Agility";
+            case StatType.Intelligence: return "Intelligence";
+            case StatType.Vitality: return "Vitality";
+
+            case StatType.AttackSpeed: return "Attack Speed";
+            case StatType.Damage: return "Damage";
+            case StatType.CritChance: return "Critical Chance";
+            case StatType.CritPower: return "Critical Power";
+            case StatType.ArmorPenetration: return "Armor Penetration";
+
+            case StatType.FireDamage: return "Fire Damage";
+            case StatType.IceDamage: return "Ice Damage";
+            case StatType.LightningDamage: return "Lightning Damage";
+            case StatType.ElementalDamage: return "Elemental Damage";
+
+            case StatType.IceResistance: return "Ice Resistance";
+            case StatType.FireResistance: return "Fire Resistance";
+            case StatType.LightningResistance: return "Lightning Resistance";
+
+            default: return "Unknown Stat";
+        }
+    }
+
+    public static bool IsPercentageStat(StatType type)
+    {
+        switch (type)
+        {
+            case StatType.CritChance:
+            case StatType.CritPower:
+            case StatType.ArmorPenetration:
+            case StatType.IceResistance:
+            case StatType.FireResistance:
+            case StatType.LightningResistance:
+            case StatType.AttackSpeed:
+            case StatType.Evasion:
+                return true;
+
+            default:
+                return false;
+        }
+    }
+}
